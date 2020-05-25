ba2fdf4d65a8c3a3376a9a7bd141b2cdaea33cf4
samir2901 <thunder.cse2018@gmail.com>
2020-05-25 21:57:58 +0530

    Simple Enemy
    Simple Enemy AI that follows the Player in a 2D Scene

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script for simple enemy that will follow the player in 2D scene (Enemy_Follow_Player)
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDist;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position,target.position) >= stoppingDist)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else
+        {            
+            Debug.Log("Game Over");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControl_TopDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script for player control in Enemy_Follow_Player Scene
+
+public class PlayerControl_TopDown : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;    
+    Vector2 movement;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+
+}
