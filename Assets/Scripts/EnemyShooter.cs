361d0bbcee3982125e06aead2d9f07598cf06c36
samir2901 <thunder.cse2018@gmail.com>
2020-05-26 22:04:37 +0530

    Enemy Shooter
    Enemy AI that allows the enemy to follow the Player and shoot at the player

---
+++ Assets/Scripts/EnemyShooter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooter : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDist;
+    public float retreatDist;
+
+    private Transform player;
+
+    private float timeBtwShots;
+    
+    public float startTimeBtwShots;
+    public GameObject projectile;     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        float dist = Vector2.Distance(transform.position, player.position);        
+        if (dist > stoppingDist)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if(dist < stoppingDist && dist > retreatDist)
+        {
+            transform.position = this.transform.position;
+        }
+        else if(dist < retreatDist)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+        if(timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
