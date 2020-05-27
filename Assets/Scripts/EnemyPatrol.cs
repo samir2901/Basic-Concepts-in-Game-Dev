cfffd980e2340c2ab1d41a5b717185d65f7c9fd1
samir2901 <thunder.cse2018@gmail.com>
2020-05-27 21:37:56 +0530

    Patrolling Enemy
    Enemy that patrols to random location.

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public float startWaitTime;
+    public Transform[] moveSpots;
+
+
+    private int randomSpot;
+    private float waitTime;
+
+    private void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+        waitTime = startWaitTime;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        if(Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
+        }
+    }
+}
