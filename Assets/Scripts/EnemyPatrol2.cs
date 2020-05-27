a431c29e8eb37f868861f2c400339be66ccc3377
samir2901 <thunder.cse2018@gmail.com>
2020-05-27 21:51:09 +0530

    Update

---
+++ Assets/Scripts/EnemyPatrol2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol2 : MonoBehaviour
+{
+    public float speed;
+    public float startWaitTime;
+    public Transform moveSpot;
+    public float minX, maxX, minY, maxY;
+
+
+    private int randomSpot;
+    private float waitTime;
+
+    private void Start()
+    {        
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxX));
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxX));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+
+        }
+    }
+}
