9d7612b968b5b86aa9f65b4c1d186d5c8e74e3f7
samir2901 <thunder.cse2018@gmail.com>
2020-05-30 11:08:29 +0530

    Tower Defence Enemy

---
+++ Assets/Scripts/EnemyPatrolTD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolTD : MonoBehaviour
+{
+    public float speed;
+    private WayPoints wayPoints;
+    private int wayPointIndex = 0;
+    private void Start()
+    {
+        wayPoints = GameObject.FindGameObjectWithTag("WayPoints").GetComponent<WayPoints>();
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints.wayPoints[wayPointIndex].position, speed * Time.deltaTime);
+        
+        Vector3 dir = wayPoints.wayPoints[wayPointIndex].position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if(Vector2.Distance(transform.position, wayPoints.wayPoints[wayPointIndex].position) < 0.1f)
+        {            
+            if(wayPointIndex <wayPoints.wayPoints.Length - 1)
+            {
+                wayPointIndex++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
